17ec3bb9620fab07acacb7721de36eb06b553555
LennyRemache <lenny.j.remache@gmail.com>
2024-06-04 19:08:11 -0400

    Rearranged pages folder structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import Home from "./components/Home";
+import Home from "./components/pages/Home";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <div>404 Not Found</div>,
+  },
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <div>404 Not Found</div>,
+  },
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <div>404 Not Found</div>,
+  },
   {
     path: "/",
     element: <Home />,
@@ -15,6 +31,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <NavBar />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
