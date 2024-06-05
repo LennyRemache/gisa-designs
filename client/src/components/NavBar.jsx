724ad2f746fb48ea96c3ed1ad833b47a6f0b488a
LennyRemache <lenny.j.remache@gmail.com>
2024-06-04 21:38:19 -0400

    Started to create the navigation bar with styling

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,19 +1,22 @@
 import { NavLink, Outlet } from "react-router-dom";
 import Logo from "../assets/gisa-designs-logo.png";
+import "../styles/NavBar.css";
 
 export default function NavBar() {
   const pages = ["", "About", "Contact", "FAQ"];
   return (
     <>
-      <nav>
+      <nav className="navbar">
         <NavLink to="/">
           <img src={Logo} alt="test" />
         </NavLink>
-        {pages.map((page, index) => (
-          <NavLink key={index} to={`/${page}`}>
-            {page === "" ? "Home" : page}
-          </NavLink>
-        ))}
+        <ul>
+          {pages.map((page, index) => (
+            <NavLink key={index} to={`/${page}`}>
+              {page === "" ? "Home" : page}
+            </NavLink>
+          ))}
+        </ul>
       </nav>
       <Outlet />
     </>
