5287977073d865ad370d24cee5d77f5c6ee1ae8f
LennyRemache <lenny.j.remache@gmail.com>
2024-06-04 20:43:26 -0400

    Set up page routes through a navigation bar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,3 +1,21 @@
+import { NavLink, Outlet } from "react-router-dom";
+import Logo from "../assets/gisa-designs-logo.png";
+
 export default function NavBar() {
-  return <div>NavBar</div>;
+  const pages = ["", "About", "Contact", "FAQ"];
+  return (
+    <>
+      <nav>
+        <NavLink to="/">
+          <img src={Logo} alt="test" />
+        </NavLink>
+        {pages.map((page, index) => (
+          <NavLink key={index} to={`/${page}`}>
+            {page === "" ? "Home" : page}
+          </NavLink>
+        ))}
+      </nav>
+      <Outlet />
+    </>
+  );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,37 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 
-import Home from "./components/pages/Home";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar.jsx";
+import Home from "./components/pages/Home.jsx";
+import About from "./components/pages/About.jsx";
+import Contact from "./components/pages/Contact.jsx";
+import FAQ from "./components/pages/FAQ.jsx";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
+    element: <NavBar />,
     errorElement: <div>404 Not Found</div>,
-  },
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <div>404 Not Found</div>,
-  },
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <div>404 Not Found</div>,
-  },
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <div>404 Not Found</div>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        errorElement: <div>404 Not Found</div>,
+      },
+      {
+        path: "/about",
+        element: <About />,
+        errorElement: <div>404 Not Found</div>,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+        errorElement: <div>404 Not Found</div>,
+      },
+      {
+        path: "/faq",
+        element: <FAQ />,
+        errorElement: <div>404 Not Found</div>,
+      },
+    ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <NavBar />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
