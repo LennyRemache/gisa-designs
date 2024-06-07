5ed18731683960c1f7b085a546091d8e690aefcb
LennyRemache <lenny.j.remache@gmail.com>
2024-06-06 21:59:19 -0400

    Started setting up home page with the hero image

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,3 +1,12 @@
+import "../../styles/pages/Home.css";
+import HeroImage from "../../assets/home-flowers-1.jpg";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="home-hero">
+        <img src={HeroImage} alt="" />
+      </div>
+    </>
+  );
 }
