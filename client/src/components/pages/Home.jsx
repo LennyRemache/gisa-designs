5e53e1dad976bbda47ac5a67669ce457e172db43
LennyRemache <lenny.j.remache@gmail.com>
2024-06-10 20:11:26 -0400

    Added text to the hero

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -5,7 +5,21 @@ export default function Home() {
   return (
     <>
       <div className="home-hero">
-        <div className="hero-text-parent">HELLO</div>
+        <div className="hero-text-parent">
+          <div className="hero-text">
+            <div className="hero-text-brand">Gisa Designs</div>
+            <div className="hero-text-service">
+              Floral Design | Event Design
+            </div>
+            <div className="hero-text-quote">
+              "Transforming dreams into stunning realities, we specialize in
+              creating unforgettable celebrations through floral artistry and
+              meticulous event design. Whether its floral arrangements or
+              intimate gatherings, let us weave elegance and beauty into every
+              moment of your special occasion."
+            </div>
+          </div>
+        </div>
         <img src={HeroImage} alt="" />
       </div>
     </>
