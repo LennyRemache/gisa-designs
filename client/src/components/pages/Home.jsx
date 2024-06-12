35a8649f2110282548c0849604224ab92fd66d93
LennyRemache <lenny.j.remache@gmail.com>
2024-06-11 20:17:55 -0400

    Added a home section and styled it

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,5 +1,8 @@
 import "../../styles/pages/Home.css";
 import HeroImage from "../../assets/home-flowers-1.jpg";
+import FlowerDesign1 from "../../assets/flower-design-1.jpeg";
+import FlowerDesign2 from "../../assets/flower-design-2.jpeg";
+import EventDesign1 from "../../assets/event-design-1.jpeg";
 
 export default function Home() {
   return (
@@ -20,8 +23,23 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <img src={HeroImage} alt="" />
+        <img src={HeroImage} alt="Hero-Image" />
       </div>
+
+      <div className="seperator">Luxury Flower Shop in NYC</div>
+      <div className="home-section-1">
+        <div className="home-section-1-desc">
+          Gisa Designs is a premier, family-owned flower shop in NYC offering
+          luxury flower delivery, subscription services, and custom arrangements
+          for corporate events, weddings, celebrations, and gift baskets.
+        </div>
+        <div className="home-section-1-photos">
+          <img src={FlowerDesign1} alt="" />
+          <img src={FlowerDesign2} alt="" />
+          <img src={EventDesign1} alt="" />
+        </div>
+      </div>
+      <div>TEST</div>
     </>
   );
 }
