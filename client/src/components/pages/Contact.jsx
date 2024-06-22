9a9fd8b719b41d77c2f39f4f81c3660bd591ed2f
LennyRemache <lenny.j.remache@gmail.com>
2024-06-21 21:22:15 -0400

    Setting up contact form component

--- client/src/components/pages/Contact.jsx
+++ client/src/components/pages/Contact.jsx
@@ -1,3 +1,22 @@
+import ContactImg from "../../assets/contact-us-image.jpeg";
+import "../../styles/pages/Contact.css";
+
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="form-parent">
+      <form className="contact-form">
+        <div className="form-details">
+          <h1>Let's bring your vision to life!</h1>
+          <p>
+            We're here to collaborate with you every step of the way. Get in
+            touch with us today to start blooming ideas together and create an
+            unforgettable experience!{" "}
+          </p>
+        </div>
+        <div className="form-image">
+          <img src={ContactImg} alt="" srcset="" />
+        </div>
+      </form>
+    </div>
+  );
 }
