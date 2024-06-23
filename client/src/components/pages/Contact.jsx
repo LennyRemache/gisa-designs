79cfbc5529a052039d2fa0739cbcd359ec6384ff
LennyRemache <lenny.j.remache@gmail.com>
2024-06-23 19:07:38 -0400

    Attempted to fix contact form styling

--- client/src/components/pages/Contact.jsx
+++ client/src/components/pages/Contact.jsx
@@ -3,20 +3,29 @@ import "../../styles/pages/Contact.css";
 
 export default function Contact() {
   return (
-    <div className="form-parent">
-      <form className="contact-form">
-        <div className="form-details">
-          <h1>Let's bring your vision to life!</h1>
-          <p>
-            We're here to collaborate with you every step of the way. Get in
-            touch with us today to start blooming ideas together and create an
-            unforgettable experience!{" "}
-          </p>
-        </div>
-        <div className="form-image">
-          <img src={ContactImg} alt="" srcset="" />
-        </div>
-      </form>
+    <div className="page-parent">
+      <div className="contact-header">
+        <div className="contact-header-title">Contact Us</div>
+        <h1>Let's bring your vision to life!</h1>
+        <p>
+          We're here to collaborate with you every step of the way. Get in touch
+          with us today to start blooming ideas together and create an
+          unforgettable experience!{" "}
+        </p>
+      </div>
+      <div className="form-parent">
+        <form className="contact-form">
+          <div className="form-details">
+            <div>
+              <label htmlFor="name">Name</label>
+              <input type="text" />
+            </div>
+          </div>
+          <div className="form-image">
+            <img src={ContactImg} alt="" srcset="" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
