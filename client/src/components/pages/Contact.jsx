abdad2d0b8347ecab184608dce7132a37f3e203f
LennyRemache <lenny.j.remache@gmail.com>
2024-06-24 18:04:25 -0400

    Further styled the contact form and added form inputs

--- client/src/components/pages/Contact.jsx
+++ client/src/components/pages/Contact.jsx
@@ -2,6 +2,15 @@ import ContactImg from "../../assets/contact-us-image.jpeg";
 import "../../styles/pages/Contact.css";
 
 export default function Contact() {
+  const services = [
+    { title: "A La Carte Menu", name: "a-la-carte-menu" },
+    { title: "Luxe Picnic", name: "lux-picnic" },
+    { title: "Proposal", name: "proposal" },
+    { title: "Special Event", name: "special-event" },
+    { title: "Flower Arrangement", name: "flower-arrangement" },
+    { title: "Other", name: "other" },
+  ];
+
   return (
     <div className="page-parent">
       <div className="contact-header">
@@ -16,9 +25,44 @@ export default function Contact() {
       <div className="form-parent">
         <form className="contact-form">
           <div className="form-details">
-            <div>
-              <label htmlFor="name">Name</label>
-              <input type="text" />
+            <div className="name-parent">
+              <p>Name</p>
+              <div className="name">
+                <div className="first-name">
+                  <label htmlFor="firstName">First Name</label>
+                  <input type="text" name="firstName" />
+                </div>
+                <div className="last-name">
+                  <label htmlFor="lastName">Last Name</label>
+                  <input type="text" name="lastName" />
+                </div>
+              </div>
+            </div>
+
+            <div className="email">
+              <label htmlFor="email">Email</label>
+              <input type="email" />
+            </div>
+            <div className="phone">
+              <label htmlFor="phone">Phone</label>
+              <input type="tel" />
+            </div>
+            <div className="services">
+              <label htmlFor="services">
+                What services are you interested in?
+              </label>
+              {services.map((service) => (
+                <div className={service.name}>
+                  <input type="checkbox" name={service.name} id="services" />{" "}
+                  <p>{service.title}</p>
+                </div>
+              ))}
+            </div>
+            <div className="date">
+              <label htmlFor="date">
+                Event Date or Delivery Date (Exact or Estimated)
+              </label>
+              <input type="date" />
             </div>
           </div>
           <div className="form-image">
