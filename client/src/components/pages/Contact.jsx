029894513b310681cae0d5f2b23e366a7d7a7da0
LennyRemache <lenny.j.remache@gmail.com>
2024-07-01 23:11:52 -0400

    Added controlled inputs to contact form

--- client/src/components/pages/Contact.jsx
+++ client/src/components/pages/Contact.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import ContactImg from "../../assets/contact-us-image.jpeg";
 import "../../styles/pages/Contact.css";
 
@@ -10,6 +11,37 @@ export default function Contact() {
     { title: "Flower Arrangement", name: "flower-arrangement" },
     { title: "Other", name: "other" },
   ];
+
+  const initialContactForm = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    services: {
+      "a-la-carte-menu": false,
+      "lux-picnic": false,
+      proposal: false,
+      "specaial-event": false,
+      "flower-arrangement": false,
+    },
+    date: "",
+    address: "",
+    city: "",
+    state: "",
+    zip: "",
+    budget: "",
+    link: "",
+    message: "",
+  };
+  const [contactForm, setContactForm] = useState(initialContactForm);
+
+  const updateContactForm = (e) => {
+    setContactForm((prevFormState) => ({
+      ...prevFormState,
+      [e.target.name]: e.target.value,
+    }));
+    console.log(contactForm);
+  };
 
   return (
     <div className="page-parent">
@@ -30,22 +62,42 @@ export default function Contact() {
               <div className="name">
                 <div className="first-name">
                   <label htmlFor="firstName">First Name</label>
-                  <input type="text" name="firstName" />
+                  <input
+                    type="text"
+                    name="firstName"
+                    value={contactForm.firstName}
+                    onChange={updateContactForm}
+                  />
                 </div>
                 <div className="last-name">
                   <label htmlFor="lastName">Last Name</label>
-                  <input type="text" name="lastName" />
+                  <input
+                    type="text"
+                    name="lastName"
+                    value={contactForm.lastName}
+                    onChange={updateContactForm}
+                  />
                 </div>
               </div>
             </div>
 
             <div className="email">
               <label htmlFor="email">Email</label>
-              <input type="email" name="email" />
+              <input
+                type="email"
+                name="email"
+                value={contactForm.email}
+                onChange={updateContactForm}
+              />
             </div>
             <div className="phone">
               <label htmlFor="phone">Phone</label>
-              <input type="tel" name="phone" />
+              <input
+                type="tel"
+                name="phone"
+                value={contactForm.phone}
+                onChange={updateContactForm}
+              />
             </div>
             <div className="services">
               <label htmlFor="services">
@@ -62,47 +114,49 @@ export default function Contact() {
               <label htmlFor="date">
                 Event Date or Delivery Date (Exact or Estimated)
               </label>
-              <input type="date" name="date" />
+              <input type="date" name="date" value={contactForm.date} />
             </div>
             <div className="address">
-              <label htmlFor="address">
-                Delivery Location / Event Location
-              </label>
+              <p>Delivery Location / Event Location</p>
               <div className="street-address">
-                <p>Address Line</p>
+                <label htmlFor="address">Address Line</label>
                 <input
                   type="text"
-                  name="address-line"
-                  id="address"
+                  name="address"
                   autoComplete="street-address"
+                  value={contactForm.address}
+                  onChange={updateContactForm}
                 />
               </div>
               <div className="city-state-zip">
                 <div>
-                  <p>City</p>
+                  <label htmlFor="city">City</label>
                   <input
                     type="text"
                     name="city"
-                    id="address"
                     autoComplete="address-level2"
+                    value={contactForm.city}
+                    onChange={updateContactForm}
                   />
                 </div>
                 <div>
-                  <p>State</p>
+                  <label htmlFor="state">State</label>
                   <input
                     type="text"
                     name="state"
-                    id="address"
                     autoComplete="address-level1"
+                    value={contactForm.state}
+                    onChange={updateContactForm}
                   />
                 </div>
                 <div>
-                  <p>Zip</p>
+                  <label htmlFor="zip">Zip</label>
                   <input
                     type="text"
                     name="zip"
-                    id="address"
                     autoComplete="postal-code"
+                    value={contactForm.zip}
+                    onChange={updateContactForm}
                   />
                 </div>
               </div>
@@ -111,8 +165,7 @@ export default function Contact() {
               <label htmlFor="budget">What is your Budget?</label>
               <select name="budget">
                 <option disabled selected>
-                  {" "}
-                  -- select an option --{" "}
+                  -- select an option --
                 </option>
                 <option>{"< $500"}</option>
                 <option>$500 - $1000</option>
@@ -127,11 +180,15 @@ export default function Contact() {
                 ( If your Pinterest Board is private, please invite us
                 @gisadesigns )
               </p>
-              <input type="url" name="pinterest-link" />
+              <input type="url" name="link" />
+            </div>
+            <div className="message">
+              <label htmlFor="message">Message</label>
+              <textarea name="message" rows="10"></textarea>
             </div>
           </div>
           <div className="form-image">
-            <img src={ContactImg} alt="" srcset="" />
+            <img src={ContactImg} alt="" />
           </div>
         </form>
       </div>
