975d5bbf0f1a599db70a396204b92c34f7effd04
LennyRemache <lenny.j.remache@gmail.com>
2024-07-27 17:36:07 -0400

    Added a configuration to express server to fix error where the req.body is undefined because it is not received into the backend from the frontebnd as a json object

--- server/server.js
+++ server/server.js
@@ -11,6 +11,8 @@ const app = express(); // initiatilzing express server side application
 // connecting server to the database
 Connect(); // needed when recieving data from front-end into the back-end that will be pushed into the db vice versa
 
+// needed to fix error where post request body is undefined and causes validationError
+app.use(express.json());
 app.use(cors()); // allows to make api calls from the client side
 
 /* ROUTES */
