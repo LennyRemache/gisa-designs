c8bc4f77660cb74e57416164cf9859f83c3821ee
LennyRemache <lenny.j.remache@gmail.com>
2024-07-27 20:28:07 -0400

    Attemped to add emailing abilities from client info on contact form

--- server/controller/requests.js
+++ server/controller/requests.js
@@ -1,6 +1,9 @@
 import { Request } from "../db/models/request.js";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
 
-// Receive new customer request info from client-side
+// Receive new customer request info from client-side and save to db and email it
 export const request = async (req, res) => {
   try {
     const {
@@ -37,7 +40,31 @@ export const request = async (req, res) => {
     });
 
     const savedRequest = await newRequest.save(); // save the new request into the DB
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail", // Use your email provider
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+    const mailOptions = {
+      replyTo: email,
+      from: "lenny.j.remache@gmail.com",
+      to: "lenny.j.remache@gmail.com",
+      subject: `Order Request #${savedRequest._id}`,
+      text: "This is a test email sent from a Node.js app!",
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
+
     res.status(201).json(savedRequest); // signal that request has be received successfully
+    res.redirect("/");
   } catch (err) {
     res.status(400).json({ err: err.message });
   }
