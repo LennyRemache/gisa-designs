52248644949620f57448d853880eb31b8d2ad1fd
LennyRemache <lenny.j.remache@gmail.com>
2024-07-27 21:32:47 -0400

    Attemped to add emailing abilities from client info on contact form

--- server/controller/requests.js
+++ server/controller/requests.js
@@ -46,8 +46,8 @@ export const request = async (req, res) => {
       port: 465,
       secure: true,
       auth: {
-        user: `${process.env.EMAIL_USER}`,
-        pass: `${process.env.EMAIL_PASS}`,
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
     const mailOptions = {
