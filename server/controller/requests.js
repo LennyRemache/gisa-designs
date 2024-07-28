e6d2daa0be51ce6d6c8f760a1c44b9f5759e52ae
LennyRemache <lenny.j.remache@gmail.com>
2024-07-27 21:17:40 -0400

    Attemped to add emailing abilities from client info on contact form

--- server/controller/requests.js
+++ server/controller/requests.js
@@ -45,6 +45,7 @@ export const request = async (req, res) => {
       service: "Gmail", // Use your email provider
       host: "smtp.gmail.com",
       port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
